0f9e634272e8d8f84c7e99448a03e58f295bc2fc
Krishnakant <kushwahakrishnakant979@gmail.com>
2026-08-18 18:42:48 +0530

    first commit2

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 import Weather from "./Weather.js";
 const App = () => {
-  const inputData = [
-    { temperature: 25, conditions: "sunny" },
-    { temperature: 56, conditions: "hot" },
-    { temperature: 12, conditions: "cold" },
-    { temperature: 30, conditions: "warm" },
-    { temperature: 18, conditions: "cloudy" },
-    { temperature: 40, conditions: "humid" },
-    { temperature: 5, conditions: "freezing" },
-    { temperature: 22, conditions: "pleasant" },
-    { temperature: 35, conditions: "dry" },
-    { temperature: 28, conditions: "windy" },
-  ];
-   
+  const [whether, setWeather] = useState({
+    temperature: 5,
+    conditions: "Sunny",
+  });
   return (
-    <span >
-      {inputData.map((ele, idx) => (
-        <Weather
-          key={idx}
-          temp={ele.temperature}
-          cond={ele.conditions}
-        ></Weather>
-      ))}
-      <p></p>
+    <span>
+      <Weather temp={whether.temperature} cond={whether.conditions}></Weather>
     </span>
   );
 };
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Weather = (props) => {
-  console.log(props);
-
+  // console.log(props);
+  const tempStyle = {
+    color: props.temp > 20 ? "red" : "blue",
+  };
   return (
     <div>
-      {<p>{props.temp}</p>}
-      {<p>{props.cond}</p>}
+      <p>
+        Temperature:<span style={tempStyle}>{props.temp}</span>
+      </p>
+      <p>Conditions: {props.cond}</p>
     </div>
   );
 };
